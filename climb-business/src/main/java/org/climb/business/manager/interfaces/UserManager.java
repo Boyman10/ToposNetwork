a3466c01db7b5b61e42ff505cae837d36e08499d
Boyman10 <contact@yasi.fr>
2018-05-23 22:17:15 +0200

    Updating the resource-ref name to match the context.xml resource name

--- climb-business/src/main/java/org/climb/business/manager/interfaces/UserManager.java
+++ climb-business/src/main/java/org/climb/business/manager/interfaces/UserManager.java
@@ -3,7 +3,9 @@ package org.climb.business.manager.interfaces;
 import java.util.List;
 
 import org.climb.model.bean.user.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserManager {
 
 	public List<User> getUsersList();
