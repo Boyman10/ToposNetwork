ff43a8d3e6ac20f85cb22ab92d5af901e82f5898
Eddy Mio <contact@remotehireman.fr>
2018-05-23 18:58:55 +0200

    Now time to write some useful methods - starting from consumer then business then from within webapp we will use the services using injection mecanisms

--- climb-business/src/main/java/org/climb/business/manager/UserManagerImpl.java
+++ climb-business/src/main/java/org/climb/business/manager/UserManagerImpl.java
@@ -1,5 +1,16 @@
 package org.climb.business.manager;
 
+import java.util.List;
+
+import org.climb.business.manager.interfaces.UserManager;
+import org.climb.model.bean.user.User;
+
 public class UserManagerImpl extends AbstractManager implements UserManager {
+
+	@Override
+	public List<User> getUsersList() {
+		
+		return getDaoFactory().getUserDao().;
+	}
 
 }
