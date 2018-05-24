8d1c03ec85b1fea1c4bd896bdb66025994c9fddb
Eddy Mio <contact@remotehireman.fr>
2018-05-24 12:13:46 +0200

    NOw on to SQL query and exception handling - interesting part :)

--- climb-consumer/src/main/java/org/climb/consumer/dao/UserDaoImpl.java
+++ climb-consumer/src/main/java/org/climb/consumer/dao/UserDaoImpl.java
@@ -7,6 +7,8 @@ import org.apache.commons.logging.LogFactory;
 import org.climb.consumer.dao.interfaces.UserDao;
 import org.climb.consumer.rm.UserRowMapper;
 import org.climb.model.bean.user.User;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.InvalidResultSetAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -42,7 +44,7 @@ public class UserDaoImpl extends AbstractDaoImpl implements UserDao {
 	 */
 	public List<User> getListUsers() {
 
-		String vSQL = "SELECT * FROM public.user";
+		String vSQL = "SELECT * FROM public.climb_user";
 
 		JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
 
@@ -61,12 +63,25 @@ public class UserDaoImpl extends AbstractDaoImpl implements UserDao {
 	@Override
 	@Transactional
 	public boolean addUser(User user) {
+
+		try {
+			LOGGER.debug("adding user to DB - UserDAO : " + user.getUsername());
+			BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(user);
+
+			return this.npjTemplate.update(
+					"INSERT INTO public.climb_user(username,email,password) values(:username,:email,:password)", params) == 1;
+			
+		} catch (InvalidResultSetAccessException e) {
+			LOGGER.error("FATAL ERROR " + e.getMessage());
+			throw new RuntimeException(e);
+		} catch (DataAccessException e) {
+			LOGGER.error("FATAL ERROR " + e.getMessage());
+			throw new RuntimeException(e);
+		}catch (Exception e) {
+			LOGGER.error("FATAL ERROR " + e.getMessage());
+			throw new RuntimeException(e);
+		}
 		
-		LOGGER.debug("adding user to DB - UserDAO : " + user.getUsername());
-		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(user);
-		
-		return this.npjTemplate.update("INSERT INTO public.user(username,email,password) values(:username,:email,:password)", params) == 1;
-		
-		
+
 	}
 }
