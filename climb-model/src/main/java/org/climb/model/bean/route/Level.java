4d38d795683c63daba80f88e8d606a5918d09ca4
Eddy Mio <contact@remotehireman.fr>
2018-06-01 09:20:12 +0200

    Working with level before implemeting Grade using Hibernate

--- climb-model/src/main/java/org/climb/model/bean/route/Level.java
+++ climb-model/src/main/java/org/climb/model/bean/route/Level.java
@@ -2,5 +2,25 @@ package org.climb.model.bean.route;
 
 public enum Level {
 
+	NOVICE("Novice"),
+	BEGIN("Beginner"),
+	INTER("Intermediate"),
+	ADVANCED("Advanced"),
+	EXPERT("Expert"),
+	SUPEXP("Super Expert"),
+	ELITE("Elite"),
+	SUPELITE("Super Elite"),
+	ALIEN("Alien");
+	
+	  private String name = "";
+	   
+	  //Constructor
+	  Level(String name){
+	    this.name = name;
+	  }
+	   
+	  public String toString(){
+	    return name;
+	  }
 	
 }
